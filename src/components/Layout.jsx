e023ee16cc3054afb65f82c7d9c8d55c3e19cd7d
bequdah <bequdah@email.com>
2026-03-03 08:35:12 +0300

    Hide footer conditionally on browse page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+    const isBrowsePage = location.pathname === '/browse';
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
@@ -10,7 +14,7 @@ const Layout = ({ children }) => {
                 {children}
             </main>
 
-            <Footer />
+            {!isBrowsePage && <Footer />}
         </div>
     );
 };
